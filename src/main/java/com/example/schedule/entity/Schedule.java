9a63ce083bb6f2550dd2b8aaa0c3e314a95eaaf6
obt <byeongtaek12@naver.com>
2025-03-24 17:09:07 +0900

    feat: Schedule클래스) 생성
    ``

---
+++ src/main/java/com/example/schedule/entity/Schedule.java
@@ -0,0 +1,15 @@
+package com.example.schedule.entity;
+
+import java.time.LocalDateTime;
+
+public class Schedule {
+    Long id;
+    String name;
+    String password;
+    String todo;
+    LocalDateTime creationdate;
+    LocalDateTime modificationdate;
+
+
+
+}

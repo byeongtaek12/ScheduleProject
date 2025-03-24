b110bd677267c95a094c5de73596cb9f03ad8992
obt <byeongtaek12@naver.com>
2025-03-24 19:32:34 +0900

    lv1로 옮김

--- src/main/java/com/example/schedule/repository/ScheduleRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.schedule.repository;
-
-import com.example.schedule.dto.ScheduleResponseDto;
-import com.example.schedule.entity.Schedule;
-
-public interface ScheduleRepository {
-    ScheduleResponseDto saveSchedule(Schedule schedule);
-}
--- src/main/java/com/example/schedule/service/ScheduleService.java
+++
@@ -1,8 +0,0 @@
-package com.example.schedule.service;
-
-import com.example.schedule.dto.ScheduleRequestDto;
-import com.example.schedule.dto.ScheduleResponseDto;
-
-public interface ScheduleService {
-    ScheduleResponseDto saveSchedule(ScheduleRequestDto dto);
-}

147c8845bb400d01e27b29bf398e2ff1be738bbf
obt <byeongtaek12@naver.com>
2025-03-25 16:42:58 +0900

    refactor: ScheduleController클래스) 수정
    password값도 받게끔 수정

--- src/main/java/com/example/schedule/controller/ScheduleController.java
+++ src/main/java/com/example/schedule/controller/ScheduleController.java
@@ -44,7 +44,7 @@ public class ScheduleController {
     // 선택 일정 수정
     @PostMapping("/{id}")
     public ResponseEntity<ScheduleResponseDto> updateSchedule(@PathVariable Long id, @RequestBody ScheduleRequestDto dto ){
-        return new ResponseEntity<>(scheduleService.updateSchedule(id,dto.getName(),dto.getTodo()),HttpStatus.OK);
+        return new ResponseEntity<>(scheduleService.updateSchedule(id,dto.getPassword(), dto.getName(),dto.getTodo()),HttpStatus.OK);
     }
 
     // 선택 일정 삭제

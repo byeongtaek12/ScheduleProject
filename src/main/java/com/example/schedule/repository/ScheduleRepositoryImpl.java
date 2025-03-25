7f4ac52a0d63c60b5f45bdc36a00c651458726aa
obt <byeongtaek12@naver.com>
2025-03-25 11:21:23 +0900

    feat: ScheduleRepositoryImpl클래스) 전체 일정 조회 메서드 생성
    데이터베이스와 연결하여 일정에 모든 데이터를 RowMapper를 통해 각 행을 일정응답객체로 변환해서 각 행을 리스트에 넣어주고
    리스트로 반환하게 해줌

--- src/main/java/com/example/schedule/repository/ScheduleRepositoryImpl.java
+++ src/main/java/com/example/schedule/repository/ScheduleRepositoryImpl.java
@@ -3,12 +3,16 @@ package com.example.schedule.repository;
 import com.example.schedule.dto.ScheduleResponseDto;
 import com.example.schedule.entity.Schedule;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
 import org.springframework.jdbc.core.namedparam.MapSqlParameterSource;
 import org.springframework.jdbc.core.simple.SimpleJdbcInsert;
 import org.springframework.stereotype.Repository;
 
 import javax.sql.DataSource;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 @Repository
@@ -36,5 +40,22 @@ public class ScheduleRepositoryImpl implements ScheduleRepository{
         Number key = simpleJdbcInsert.executeAndReturnKey(new MapSqlParameterSource(parameters));
         return new ScheduleResponseDto(key.longValue(), schedule.getName(),schedule.getPassword(), schedule.getTodo(),
                 schedule.getCreationdate(),schedule.getCreationdate());
+    }
+
+    @Override
+    public List<ScheduleResponseDto> findAllSchedules() {
+        return jdbcTemplate.query("select * from schedule", scheduleRowMapper());
+    }
+
+    private RowMapper<ScheduleResponseDto> scheduleRowMapper(){
+        return new RowMapper<ScheduleResponseDto>() {
+            @Override
+            public ScheduleResponseDto mapRow(ResultSet rs, int rowNum) throws SQLException {
+                return new ScheduleResponseDto(rs.getLong("id"), rs.getString("name"),
+                        rs.getString("password"),rs.getString("todo"),
+                        rs.getTimestamp("creationdate").toLocalDateTime(),
+                        rs.getTimestamp("modificationdate").toLocalDateTime());
+            }
+        };
     }
 }

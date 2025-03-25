cb4e26b037cae93c1561ba1a571bee337d7901f8
obt <byeongtaek12@naver.com>
2025-03-25 14:42:35 +0900

    refactor: ScheduleResponseDto클래스) 응답객체 멤버 삭제 및 생성자 추가
    응답객체 멤버 중에 password는 반환하면 안 되서 삭제해줬고, 일정을 받는 생성자도 필요해서 추가해줌

--- src/main/java/com/example/schedule/dto/ScheduleResponseDto.java
+++ src/main/java/com/example/schedule/dto/ScheduleResponseDto.java
@@ -1,5 +1,6 @@
 package com.example.schedule.dto;
 
+import com.example.schedule.entity.Schedule;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import java.time.LocalDateTime;
@@ -9,9 +10,13 @@ import java.time.LocalDateTime;
 public class ScheduleResponseDto {
     private Long id;
     private String name;
-    private String password;
     private String todo;
     private LocalDateTime creationdate;
     private LocalDateTime modificationdate;
 
+    public ScheduleResponseDto(Schedule schedule) {
+        this.id=schedule.getId();
+        this.name=schedule.getName();
+        this.todo=schedule.getTodo();
+    }
 }

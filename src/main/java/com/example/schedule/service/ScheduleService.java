06f02d16ea964efa8803dc89b9c8d298cc4c0742
obt <byeongtaek12@naver.com>
2025-03-25 11:21:48 +0900

    feat: ScheduleService인터페이스) 메서드 생성
    전체 일정 조회하는 메서드 생성

--- src/main/java/com/example/schedule/service/ScheduleService.java
+++ src/main/java/com/example/schedule/service/ScheduleService.java
@@ -3,6 +3,9 @@ package com.example.schedule.service;
 import com.example.schedule.dto.ScheduleRequestDto;
 import com.example.schedule.dto.ScheduleResponseDto;
 
+import java.util.List;
+
 public interface ScheduleService {
     ScheduleResponseDto saveSchedule(ScheduleRequestDto dto);
+    List<ScheduleResponseDto> findAllSchedules();
 }

d4955002bfe052984e0908567bc017953a4cdbf6
obt <byeongtaek12@naver.com>
2025-03-25 11:06:03 +0900

    refactor: ScheduleResponseDto클래스) 생성자 변경
    모든 멤머 변수를 초기화하도록 @AllArgsConstructor 선언

--- src/main/java/com/example/schedule/dto/ScheduleResponseDto.java
+++ src/main/java/com/example/schedule/dto/ScheduleResponseDto.java
@@ -1,7 +1,9 @@
 package com.example.schedule.dto;
 
+import lombok.AllArgsConstructor;
 import java.time.LocalDateTime;
 
+@AllArgsConstructor
 public class ScheduleResponseDto {
     private Long id;
     private String name;
@@ -10,10 +12,4 @@ public class ScheduleResponseDto {
     private LocalDateTime creationdate;
     private LocalDateTime modificationdate;
 
-    public ScheduleResponseDto(Long id, String name, String password, String todo) {
-        this.id=id;
-        this.name=name;
-        this.password=password;
-        this.todo=todo;
-    }
 }

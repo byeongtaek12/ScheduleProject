ed6f1593a993caa24234a02960654cef98eae9e4
obt <byeongtaek12@naver.com>
2025-03-25 14:39:52 +0900

    feat: ScheduleRepository인터페이스) 메서드 생성
    단건 조회하는 메서드 생성

--- src/main/java/com/example/schedule/repository/ScheduleRepository.java
+++ src/main/java/com/example/schedule/repository/ScheduleRepository.java
@@ -7,4 +7,5 @@ import java.util.List;
 public interface ScheduleRepository {
     ScheduleResponseDto saveSchedule(Schedule schedule);
     List<ScheduleResponseDto> findAllSchedules();
+    Schedule findScheduleByIdOrElseThrow(Long id);
 }

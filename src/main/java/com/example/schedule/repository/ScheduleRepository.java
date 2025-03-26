930f64bd1e67d657521646bb9c3717f77af9d32b
obt <byeongtaek12@naver.com>
2025-03-26 12:08:11 +0900

    refactor: ScheduleRepository인터페이스) 메서드 수정
    일정 목록 조회하는 메서드 부분에서 lv1요구사항 빼먹은 것이 있어서 요구사항에 맞게 수정

--- src/main/java/com/example/schedule/repository/ScheduleRepository.java
+++ src/main/java/com/example/schedule/repository/ScheduleRepository.java
@@ -2,11 +2,13 @@ package com.example.schedule.repository;
 
 import com.example.schedule.dto.ScheduleResponseDto;
 import com.example.schedule.entity.Schedule;
+
+import java.time.LocalDateTime;
 import java.util.List;
 
 public interface ScheduleRepository {
     ScheduleResponseDto saveSchedule(Schedule schedule);
-    List<ScheduleResponseDto> findAllSchedules();
+    List<ScheduleResponseDto> findAllSchedules(String name, String modificationdate);
     Schedule findScheduleByIdOrElseThrow(Long id);
     int updateSchedule(Long id,String password, String name,String todo);
     int deleteSchedule(Long id, String password);
